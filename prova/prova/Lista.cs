f0fa519c5078b9f87a1b160b7f49cd4741ddeae3
Famiglia Albi <Famiglia Albi>
2017-03-16 16:50:59 +0100

    lista

---
+++ prova/prova/Lista.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova
+{ 
+    class Lista                                                            //per creare una lista 
+    { 
+        public List<Uomo> elenco = new List<Uomo>();
+
+        public void aggiungi(Uomo u)
+        {
+            elenco.Add(u);
+        }
+    }
+}
+}
